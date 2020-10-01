66bdb17ad02bc919817e33d84b5adab34cd0b35f
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-10-01 19:58:19 +0100

    Complete tests

--- jest-test-exercises/my-attempts/14 - Promise Testing/index.js
+++ jest-test-exercises/my-attempts/14 - Promise Testing/index.js
@@ -23,43 +23,22 @@ export const defaultFetchHeaders = {
     },
 };
 
-// https://www.typescriptlang.org/play?#code/MYewdgziA2CmB0w4EMBOAKAlAbgLACgAXATwAdYACEAIwCsKBeCk8kAMyrr31EkM-pMA3gQpiKwAJYkAXBQBEAW2IBhacXkAaUeOQATPalgQIcpcQCCBoya06x7NpODG5I-OM9UwsM09QQhADybE4udh5eYoQA7iBmELC8eiFhsPL2FAC+BDn4BLyBFEbAAK4BsABKsHqlLowU6DS0cs1JhJitdO2MAHwU7p5GhOVgFEHdwITwsGCEqJLGTXSY8Ea1LujoyMDAmhQA2gDWsMT7AG7I0AC6mH0DmZ6F-IogepJONQDSpw0nxPBCCAADIgGKwVAqZCJLDcKJiZ4UV7vT56ABqVwaLFg7Aol2gjAYTHkbSm8goADIKXjMQBCIkUMClaAEgD8xSS5US1Q2sHQ+Lucnx3Ee4mGowekXh4ngsp2e1FUVl8CEh2RH0Weh+xGucnVqIxBLy0uN4iy+yEWRwuRFPHARQAHg0SlyqjU6nzmta7ZAYAhoCAAOboB04IA
-// export const camelCase = (input) => {
-//     // TODO: implement
-//     // return input;
-//     if (Array.isArray(input)) {
-//         return input;
-//     }
-
-//     return Object.entries(input).reduce((acc, [key, val]) => {
-//         const modifiedKey = key.replace(/^\w/, (match) => match.toLowerCase());
-//         const modifiedVal =
-//             typeof val === "object" && val !== null ? camelCase(val) : val;
-
-//         return {
-//             ...acc,
-//             ...{ [modifiedKey]: modifiedVal },
-//         };
-//     }, {});
-// };
-
-// const normalizeCasing = (value) => {
-//     // TODO: implement
-// };
 export const camelCase = (input) => {
-    // TODO: implement
-    // return input;
-    return Object.entries(input).reduce((acc, [key, val]) => {
-        const modifiedKey = key.replace(/^\w/, (match) => match.toLowerCase());
+    if (Array.isArray(input)) {
+        const normalizedItems = input.map((item) => {
+            return !Array.isArray(item) && typeof item === "object"
+                ? normalizeCasing(item)
+                : item;
+        });
+        return normalizedItems;
+    }
 
-        if (Array.isArray(val)) {
-            const modifiedArray = val.map((entry) => {
-                return typeof entry === "object" && entry !== null 
-                    ? camelCase(entry)
-                    : entry;
-            });
-            return { ...acc, [modifiedKey]: modifiedArray };
-        }
+    return normalizeCasing(input);
+};
+
+const normalizeCasing = (value) => {
+    return Object.entries(value).reduce((acc, [key, val]) => {
+        const modifiedKey = key.replace(/^\w/, (match) => match.toLowerCase());
 
         const modifiedVal =
             typeof val === "object" && val !== null ? camelCase(val) : val;
@@ -71,18 +50,13 @@ export const camelCase = (input) => {
     }, {});
 };
 
-const normalizeCasing = (value) => {
-    // TODO: implement
-};
-
 const callApi = (url = "", options = {}) => {
-    // TODO: implement
-
     const request = fetch(LOCATION_ORIGIN + url, {
         ...defaultFetchHeaders,
     });
 
     const response = request
+
         .then((result) => {
             let retObj = {
                 resp: result,
