b8cedde5b009e5a61faa80f1a93eae09f68a767a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-10-10 19:38:18 +0100

    Add mock for function returns

--- youtube-logrocket/src/__tests__/tests.test.js
+++ youtube-logrocket/src/__tests__/tests.test.js
@@ -20,3 +20,26 @@ for (let i = 0; i < pizzas.length; i++) {
         expect(pizzas[i]).toHaveProperty("price");
     });
 }
+
+test("should mock implementation of a basic function", () => {
+    const mock = jest.fn(() => "I am a mock function");
+    expect(mock("calling my mock function")).toBe("I am a mock function");
+    expect(mock).toHaveBeenCalledWith("calling my mock function");
+});
+
+test("should mock return value of a function one time", () => {
+    const mock = jest.fn();
+    mock.mockReturnValueOnce("hello").mockReturnValueOnce("there");
+
+    // console.log(mock(), mock());
+    mock(); // "hello"
+    mock(); // "there"
+
+    expect(mock).toHaveBeenCalledTimes(2);
+
+    mock("hello", "there", "brian");
+    expect(mock).toHaveBeenCalledWith("hello", "there", "brian");
+
+    mock("brian");
+    expect(mock).toHaveBeenLastCalledWith("brian")
+});
